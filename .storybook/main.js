d89b76330f8f324554c77c9b6e56e4610156d5f6
Leon <qinlanfeng@foxmail.com>
2021-01-27 21:27:19 +0800

    import sass config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,7 @@
+const path = require('path');
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -6,5 +10,22 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    config.resolve.alias['@packages'] = resolve('../packages')
+
+    // Return the altered config
+    return config;
+  },
 }
